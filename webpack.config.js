5eed3c6b4fb358774cd84b894da8f686f43ef00d
suzuxander <suzuxander@gmail.com>
2022-06-23 11:02:24 +0900

    Add tsconfig.json

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+    watchOptions: {
+        poll: true
+    },
+    mode: 'development',
+    entry: {
+        client: './app/client.ts',
+        // test: './app/test.ts',
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js',
+        libraryTarget: 'commonjs2',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)?$/,
+                exclude: /node_modules/,
+                use: ['ts-loader']
+            },
+        ]
+    },
+    plugins: [],
+    resolve: {
+        extensions: ['.ts', '.js', '.json'],
+        modules: [
+            'node_modules',
+            'app',
+            'gen'
+        ],
+        alias: {
+            gen: path.resolve(__dirname, './gen'),
+            app: path.resolve(__dirname, './app')
+        },
+    },
+    target: 'node'
+};
