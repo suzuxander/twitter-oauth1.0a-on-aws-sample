1d3eea7e2d923818582d1b2f287ddf0041ba4c09
suzuxander <suzuxander@gmail.com>
2022-06-23 11:03:28 +0900

    Add script

---
+++ script/init.sh
@@ -0,0 +1,13 @@
+npm i
+
+echo ACCOUNT_ID= >> .env
+echo REGION= >> .env
+echo API_KEY= >> .env
+echo API_KEY_SECRET= >> .env
+echo BUCKET= >> .env
+echo CALLBACK_URL= >> .env
+echo API_BASE_PATH= >> .env
+
+cd cdk
+npm i
+
